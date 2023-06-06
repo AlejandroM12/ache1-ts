25108eedeb2c1784f6de295e76868a39f14a0fd7
alejandrom12 <alejandro.morel1905@gmail.com>
2023-06-06 19:45:59 -0300

    feat: Add routes with react-router-dom and test Header with Navbar. This commit implements the necessary routes using react-router-dom for navigation within the application. Additionally, the Header component has been tested with the Navbar component to ensure proper functionality and navigation between the created routes.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Header, Navbar } from "@/components";
+
+const Home = () => {
+  return (
+    <div>
+      <Header transparent={true} height={100}>
+        <Navbar />
+      </Header>
+    </div>
+  );
+};
+
+export default Home;
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,4 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, Contact, Home, Jobs, Services } from "@/pages";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
